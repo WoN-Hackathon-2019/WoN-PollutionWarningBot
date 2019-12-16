9926f02cb2460baa6026d68a4970bb646afc7328
Marc-Goritschnig(Ultrabook) <marc.goritschnig1@gmail.com>
2019-12-16 09:54:14 +0100

    New data

--- src/main/java/won/bot/skeleton/action/MatcherExtensionAtomCreatedAction.java
+++ src/main/java/won/bot/skeleton/action/MatcherExtensionAtomCreatedAction.java
@@ -13,6 +13,7 @@ import won.bot.framework.eventbot.event.Event;
 import won.bot.framework.eventbot.listener.EventListener;
 import won.bot.framework.extensions.matcher.MatcherExtensionAtomCreatedEvent;
 import won.bot.skeleton.context.SkeletonBotContextWrapper;
+import won.bot.skeleton.impl.AirQualityDataSchema;
 import won.protocol.message.WonMessage;
 import won.protocol.message.builder.WonMessageBuilder;
 import won.protocol.util.DefaultAtomModelWrapper;
@@ -54,7 +55,7 @@ public class MatcherExtensionAtomCreatedAction extends BaseEventBotAction {
 
         DefaultAtomModelWrapper defaultWrapper = new DefaultAtomModelWrapper(atomCreatedEvent.getAtomData());
         Collection<String> tags = defaultWrapper.getAllTags();
-        if(!tags.contains("AirData")) {//todo: filter tags set in atoms of AirQualityBot
+        if(!tags.contains("AirQualityData")) {//todo: filter tags set in atoms of AirQualityBot
             return;
         }
 
@@ -110,28 +111,34 @@ public class MatcherExtensionAtomCreatedAction extends BaseEventBotAction {
         Model m = defaultWrapper.getAtomModel();
         Resource atom = defaultWrapper.getAtomModel().getResource(((MatcherExtensionAtomCreatedEvent) event).getAtomURI().toString());
         String def_string = "We registered that an Atom w/ tag 'AirData' was created, atomUri is: " + atomCreatedEvent.getAtomURI()+ "\n";
-        String city = "City: Wien\n";
 
         Property ozone = m.createProperty("http://schema.org/ozone");
-        Property nitrogen = m.createProperty("http://schema.org/ozone");
-        Property sulfur = m.createProperty("http://schema.org/ozone");
-        Property carbon = m.createProperty("http://schema.org/ozone");
-        Property pm10 = m.createProperty("http://schema.org/ozone");
-        Property pm2_5 = m.createProperty("http://schema.org/ozone");
-        Property tsp = m.createProperty("http://schema.org/ozone");
 
-        String airQ1 = "Air quality index (Ozone): " + getCat(atom.getProperty(ozone).getInt(), PPM_OZONE) + "\n";
-        String airQ2 = "Air quality index (Nitrogen dioxide): " + getCat(atom.getProperty(nitrogen).getInt(), PPM_NO) + "\n";
-        String airQ3 = "Air quality index (Sulfur dioxide): " + getCat(atom.getProperty(sulfur).getInt(), PPM_SDIO) + "\n";
-        String airQ4 = "Air quality index (Carbon monoxide): " + getCat(atom.getProperty(carbon).getInt(), PPM_CO) + "\n";
-        String airQ5 = "Air quality index (PM10): " + getCat(atom.getProperty(pm10).getInt(), µG_PM10) + "\n";
-        String airQ6 = "Air quality index (PM2.5): " + getCat(atom.getProperty(pm2_5).getInt(), µG_PM2_5) + "\n";
-        String airQ7 = "Air quality index (TSP): " + getCat(atom.getProperty(tsp).getInt(), µG_TSP) + "\n";
+        String location = "Location: " + atom.getProperty(AirQualityDataSchema.LOCATION) + "\n";
+        String city = "City: " + atom.getProperty(AirQualityDataSchema.CITY) + "\n";
+        String country = "Country: " + atom.getProperty(AirQualityDataSchema.COUNTRY) + "\n";
+        String mParam = "Measurements: " + atom.getProperty(AirQualityDataSchema.MEASUREMENTS).getProperty(AirQualityDataSchema.MEASURE_PARAM) + "\n";
+        String mValue = "Measurements: " + atom.getProperty(AirQualityDataSchema.MEASUREMENTS).getProperty(AirQualityDataSchema.MEASURE_VALUE) + "\n";
+        String mUnit = "Measurements: " + atom.getProperty(AirQualityDataSchema.MEASUREMENTS).getProperty(AirQualityDataSchema.MEASURE_UNIT) + "\n";
+
+        //String airQ1 = "Air quality index (Ozone): " + getCat(atom.getProperty(ozone).getInt(), PPM_OZONE) + "\n";
+        //String airQ2 = "Air quality index (Nitrogen dioxide): " + getCat(atom.getProperty(nitrogen).getInt(), PPM_NO) + "\n";
+        //String airQ3 = "Air quality index (Sulfur dioxide): " + getCat(atom.getProperty(sulfur).getInt(), PPM_SDIO) + "\n";
+        //String airQ4 = "Air quality index (Carbon monoxide): " + getCat(atom.getProperty(carbon).getInt(), PPM_CO) + "\n";
+        //String airQ5 = "Air quality index (PM10): " + getCat(atom.getProperty(pm10).getInt(), µG_PM10) + "\n";
+        //String airQ6 = "Air quality index (PM2.5): " + getCat(atom.getProperty(pm2_5).getInt(), µG_PM2_5) + "\n";
+        //String airQ7 = "Air quality index (TSP): " + getCat(atom.getProperty(tsp).getInt(), µG_TSP) + "\n";
 
         //String description1 = "description: " + atom.getProperty(SCHEMA.DESCRIPTION).getResource() + "\n";
         //String description2 = "description: " + atom.getProperty(SCHEMA.DESCRIPTION).getLanguage() + "\n";
 
-        return def_string.concat(city).concat(airQ1).concat(airQ2).concat(airQ3).concat(airQ4).concat(airQ5).concat(airQ6).concat(airQ7);
+        return def_string.concat(city)
+                .concat(location)
+                .concat(city)
+                .concat(country)
+                .concat(mParam)
+                .concat(mValue)
+                .concat(mUnit);
     }
 
     private String getCat(float value, float opt){
